fec2fa90cefd3078d9eab4ee18df001b3a3635b1
Quyet <huuquyetng@gmail.com>
2024-04-15 17:00:22 +0700

    chore: rename files

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,9 @@ const nextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: './dist', // Changes the build output directory to `./dist/`.
 
+  compiler : {
+    styledComponents: true,
+  },
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   // Override the default webpack configuration
   webpack: (config) => {
